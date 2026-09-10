bbda4824b470875b17e16f6b540a95dbddfd1dc4
gorlelokesh25csm-afk <gorlelokesh.25.csm@anits.edu.in>
2026-09-10 21:04:26 +0530

    Create Contains.java

---
+++ Assignment/Assignment 3/Strings/Contains.java
@@ -0,0 +1,14 @@
+
+public class StringsProg {
+    public static void main(String[] args) {
+String str = "lokesh is an artist";
+
+System.out.println(str.contains("artist"));
+
+
+
+
+
+    }
+    
+}
