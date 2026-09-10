45a58855b136155e23649ed5c8f0555599e4d07c
gorlelokesh25csm-afk <gorlelokesh.25.csm@anits.edu.in>
2026-09-10 21:03:28 +0530

    Create Concat.java

---
+++ Assignment/Assignment 3/Strings/Concat.java
@@ -0,0 +1,11 @@
+
+public class StringsProg {
+    public static void main(String[] args) {
+String s1 = "lokesh ";
+String s2 = " gorle";
+
+System.out.println(s1.concat(s2));    }
+    
+}
+
+    
