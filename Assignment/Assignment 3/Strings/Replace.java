d7178fc500e953c36d97dc7f7b2b6213b52c4496
gorlelokesh25csm-afk <gorlelokesh.25.csm@anits.edu.in>
2026-09-10 21:08:42 +0530

    Create Replace.java

---
+++ Assignment/Assignment 3/Strings/Replace.java
@@ -0,0 +1,13 @@
+
+public class StringsProg {
+    public static void main(String[] args) {
+String str = "lokesh is an artist";
+System.out.println(str.replace("an artist", "a cricket player"));
+
+
+
+
+
+    }
+    
+}
