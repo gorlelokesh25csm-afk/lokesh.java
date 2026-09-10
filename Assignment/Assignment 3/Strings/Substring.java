f6628a7fddcca04502c27de5b08bdc828fdd50c4
gorlelokesh25csm-afk <gorlelokesh.25.csm@anits.edu.in>
2026-09-10 21:09:42 +0530

    Create Substring.java

---
+++ Assignment/Assignment 3/Strings/Substring.java
@@ -0,0 +1,11 @@
+
+public class StringsProg {
+    public static void main(String[] args) {
+String str = "lokesh gorle";
+
+System.out.println(str.substring(6));
+System.out.println(str.substring(0, 5));
+
+    }
+    
+}
